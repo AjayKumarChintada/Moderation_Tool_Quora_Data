8018bb884deb1c4c746279540599a6f2b7c6d929
AjayKumarChintada <106527457+AjayKumarChintada@users.noreply.github.com>
2023-03-03 17:24:48 +0530

    session global dic

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import pandas as pd
 app = Flask(__name__)
 app.secret_key = 'LETS-KEEP-SOME-SECRET-HERE'
 
+sessions = {}
+
 def get_status(row):
     if row['feedback_yes_count'] >= 3:
         return 'Yes'
@@ -13,38 +15,46 @@ def get_status(row):
         return 'No'
     else:
         return 'Hey'
-status_changed_rows =[]
+
+
+status_changed_rows = []
 def get_rows():
     df = pd.read_csv('dataset.csv')
     feedback_cols = df.filter(regex='^feedback')
     df['feedback_yes_count'] = feedback_cols.eq('Yes').sum(axis=1)
     df['feedback_no_count'] = feedback_cols.eq('No').sum(axis=1)
     if 'status' not in df.columns:
-        return df.to_dict(orient='records'),[]
-    return df[df['status'] == 'Hey'].to_dict(orient='records'),df[df['status'] != 'Hey'].to_dict(orient='records')
+        return df.to_dict(orient='records'), []
+    return df[df['status'] == 'Hey'].to_dict(orient='records'), df[df['status'] != 'Hey'].to_dict(orient='records')
+
 
 @app.route("/", methods=["GET", "POST"])
-def feedback():  # sourcery skip: assign-if-exp, switch
-
+def feedback(): 
     try:
-
         global status_changed_rows
         if request.method == "GET":
-            rows,status_changed_rows = get_rows()
+            uuid_value = str(uuid.uuid4())
+
+            rows, status_changed_rows = get_rows()
             # session['status_changed_rows'] = status_changed_rows
             if rows:
-                session["rows"] = rows
+
+                # session["rows"] = rows
+
                 session["row_index"] = 0
-                session["uuid"] = str(uuid.uuid4())
+                session["uuid"] = uuid_value
+                sessions[uuid_value] = {'rows': rows}
+
                 uuid_key = f"feedback_{session['uuid']}"
                 return render_template("feedback.html", row=rows[0])
             return render_template("completed.html")
 
         elif request.method == "POST":
-            
+
             feedback = request.form["feedback"]
             uuid_key = f"feedback_{session['uuid']}"
-            rows = session["rows"]
+            print(sessions)
+            rows = sessions[session['uuid']]['rows']
             row_index = session["row_index"]
             rows[row_index][uuid_key] = feedback
             if rows[row_index][uuid_key] == 'Yes':
@@ -62,8 +72,10 @@ def feedback():  # sourcery skip: assign-if-exp, switch
                 return render_template("feedback.html", row=rows[session["row_index"]])
             else:
                 return "Thank you for your feedback!"
-    except:
-        return "Some error occured :( .,refresh or try again later"
+            
+    except Exception:
+        return "Some error occured:(.. Refresh or comback again!"+ str(Exception)
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001, host='0.0.0.0')
-
